83ab7b663f8944185269831ad913ea747b02e7fa
taha uygun <tahauygun@gmail.com>
2019-01-30 01:10:24 -0800

    first commit

---
+++ VoiceClassification.py
@@ -0,0 +1,35 @@
+# Ignore  the warnings
+import warnings
+warnings.filterwarnings('always')
+warnings.filterwarnings('ignore')
+
+# data visualisation and manipulation
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+import seaborn as sns
+import missingno as msno
+
+style.use('fivethirtyeight')
+sns.set(style='whitegrid',color_codes=True)
+
+#import the necessary modelling algos.
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn import datasets
+from sklearn.naive_bayes import GaussianNB
+
+#model selection
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score,precision_score,recall_score,confusion_matrix,roc_curve,roc_auc_score
+from sklearn.model_selection import GridSearchCV
+
+#preprocess.
+from sklearn.preprocessing import MinMaxScaler,StandardScaler,Imputer,LabelEncoder,OneHotEncoder
